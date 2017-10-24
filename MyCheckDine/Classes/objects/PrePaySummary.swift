274db46c051f4c315dc3c44c427ba519bdaee485
elad schiller <eladsc@mycheck.co.il>
2017-10-24 13:31:50 +0300

    BL-268 changed param names

--- MyCheckDine/Classes/objects/PrePaySummary.swift
+++ MyCheckDine/Classes/objects/PrePaySummary.swift
@@ -42,8 +42,8 @@ public struct PrePaySummary{
   internal init?(json: JSON) {
     
     guard let tax: Double = "totalTax" <~~ json,
-    let subtotal: Double = "subtotal" <~~ json ,
-    let total: Double = "total" <~~ json else{
+    let subtotal: Double = "totalBeforeTax" <~~ json ,
+    let total: Double = "totalAfterTax" <~~ json else{
       return nil
     }
     self.taxValue = Money(value: tax)
